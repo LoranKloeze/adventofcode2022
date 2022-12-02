719dbeb02e3674fbe9b2f3ba4818ff1887968db4
Loran Kloeze <loran@codedivision.nl>
2022-12-02 17:27:48 +0100

    More test for day 2, part 2

--- day2/part_two_test.go
+++ day2/part_two_test.go
@@ -44,3 +44,40 @@ func TestRealForTwo(t *testing.T) {
 	}
 
 }
+
+func TestParseActualMove(t *testing.T) {
+
+	type testCase struct {
+		opponentPlays string
+		mePlays       string
+		expScore      int
+	}
+
+	tests := []testCase{
+		{opponentPlays: "A", mePlays: "X", expScore: 3},
+		{opponentPlays: "A", mePlays: "Y", expScore: 4},
+		{opponentPlays: "A", mePlays: "Z", expScore: 8},
+
+		{opponentPlays: "B", mePlays: "X", expScore: 1},
+		{opponentPlays: "B", mePlays: "Y", expScore: 5},
+		{opponentPlays: "B", mePlays: "Z", expScore: 9},
+
+		{opponentPlays: "C", mePlays: "X", expScore: 2},
+		{opponentPlays: "C", mePlays: "Y", expScore: 6},
+		{opponentPlays: "C", mePlays: "Z", expScore: 7},
+	}
+
+	for _, c := range tests {
+		if got, _ := parseActualMove(c.opponentPlays, c.mePlays); got != c.expScore {
+			t.Errorf("Expected parseActualMove(%q,%q) to return %d, got %d", c.opponentPlays, c.mePlays, c.expScore, got)
+		}
+	}
+}
+
+func TestUnknownActualMove(t *testing.T) {
+	opponent, me := "foo", "bar"
+	_, err := parseActualMove(opponent, me)
+	if err == nil {
+		t.Errorf("Expected call parseMove(%q,%q) to return error", opponent, me)
+	}
+}
