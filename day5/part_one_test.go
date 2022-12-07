7f711f75984d49815022dbf456cb6613e6760444
Loran Kloeze <loran@codedivision.nl>
2022-12-07 20:27:01 +0100

    Rough solution for day 5, part 1

---
+++ day5/part_one_test.go
@@ -0,0 +1,51 @@
+// Copyright 2022 Loran Kloeze. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package day5
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestSampleForOne(t *testing.T) {
+	const sample = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+	1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+`
+
+	b := bytes.NewBufferString(sample)
+	got := resultOfCrateMover9000(b)
+	exp := "CMZ"
+
+	if got != exp {
+		t.Errorf("Wrong top rows of stacks, expected %s, got %s", exp, got)
+	}
+}
+
+func TestRealForOne(t *testing.T) {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		t.Fatalf("Failed to open input data: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	got := resultOfCrateMover9000(f)
+
+	// Your answer is probably different
+	exp := "RFFFWBPNS"
+
+	if got != exp {
+		t.Errorf("Wrong top rows of stacks, expected %s, got %s", exp, got)
+	}
+
+}
